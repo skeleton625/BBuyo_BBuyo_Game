a8eeed8c2f0c781f11d74aad0fa255e8a8946126
skeleton625 <chaos0824@naver.com>
2019-11-29 21:25:11 +0900

    블록 집합 자식 클래스(접힌, 십자가, 막대기 블록)들 구현 완료

--- BBuyo_BBuyo/cross_block.cpp
+++ BBuyo_BBuyo/cross_block.cpp
@@ -1,22 +1,24 @@
-#pragma once
 #include "cross_block.h"
+#include "array_2d.h"
+#include "block.h"
 
-cross_block::cross_block(int c1, int c2, int c3, int c4, int c5) : big_block(CROSS)
+cross_block::cross_block(int c1, int c2, int c3, int c4, int c5) : big_block(2)
 {
 	block* b1 = new block(c1);
-	b1->set_location(0, 3);
 	block* b2 = new block(c2);
-	b2->set_location(1, 2);
 	block* b3 = new block(c3);
-	b3->set_location(2, 2);
 	block* b4 = new block(c4);
-	b4->set_location(3, 2);
 	block* b5 = new block(c5);
-	b5->set_location(2, 3);
+	b1->set_location(1, 3);
+	b2->set_location(2, 2);
+	b3->set_location(2, 3);
+	b4->set_location(2, 4);
+	b5->set_location(3, 3);
 
-	v.push_back(b1);
+	v.push_back(b5);
 	v.push_back(b2);
 	v.push_back(b3);
 	v.push_back(b4);
-	v.push_back(b5);
+	v.push_back(b1);
+	array_2d::insert(v);
 }
--- BBuyo_BBuyo/tree_block.cpp
+++ BBuyo_BBuyo/tree_block.cpp
@@ -1,15 +1,18 @@
 #include "tree_block.h"
+#include "array_2d.h"
+#include "block.h"
 
-tree_block::tree_block(int c1, int c2, int c3) : big_block(TREE)
+tree_block::tree_block(int c1, int c2, int c3) : big_block(1)
 {
 	block* b1 = new block(c1);
-	b1->set_location(0, 3);
 	block* b2 = new block(c2);
-	b2->set_location(1, 3);
 	block* b3 = new block(c3);
-	b3->set_location(2, 3);
+	b1->set_location(1, 3);
+	b2->set_location(2, 3);
+	b3->set_location(3, 3);
 
-	v.push_back(b1);
-	v.push_back(b2);
 	v.push_back(b3);
+	v.push_back(b2);
+	v.push_back(b1);
+	array_2d::insert(v);
 }
