1efb65252b8229affc79895fd215c07d7914f328
skeleton625 <chaos0824@naver.com>
2019-11-29 21:26:15 +0900

    같은 색끼리 붙어있는 블록 집합 클래스 구현 완료

--- BBuyo_BBuyo/color_block.cpp
+++ BBuyo_BBuyo/color_block.cpp
@@ -1,18 +1,29 @@
 #include "block.h"
+#include "array_2d.h"
 #include "color_block.h"
 
 // 같은 색 모임 객체 생성자
 color_block::color_block(block* b)
 {
 	s = set<block*>();
-	s.insert(b);
+	if (b != NULL)
+		s.insert(b);
 }
 
 // 해당 객체의 같은 색 모임에 매개변수 같은 색 모임을 합하는 함수
-void color_block::insert(set<block*> s)
+void color_block::insert(set<block*> blocks)
 {
-	for (block* b : s)
+	for (block* b : blocks)
+	{
+		b->set_group(this);
 		s.insert(b);
+	}
+		
+}
+
+void color_block::remove(block* b)
+{
+	s.erase(b);
 }
 
 // 해당 객체의 같은 색 모임 반환 함수
@@ -20,10 +31,3 @@ set<block*> color_block::get_set() { return s; }
 
 // 해당 객체의 같은 색 모임 숫자 반환 함수
 int color_block::get_set_size() { return s.size(); }
-
-// 해당 객체의 같은 색 모임에 있는 모든 블록을 삭제하는 함수
-void color_block::explosion()
-{
-	for (block* b : s)
-		b->set_color(0);
-}
