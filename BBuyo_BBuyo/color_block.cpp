da939c784094ece1459eb0d79b7d6b601cb3afc9
skeleton625 <chaos0824@naver.com>
2019-12-09 15:43:57 +0900

    블록의 블록 집합 색 함수 구현

--- BBuyo_BBuyo/color_block.cpp
+++ BBuyo_BBuyo/color_block.cpp
@@ -3,9 +3,10 @@
 #include "color_block.h"
 
 // 같은 색 모임 객체 생성자
-color_block::color_block(block* b)
+color_block::color_block(block* b, int color)
 {
-	s = set<block*>();
+	this->color = color;
+	this->s = set<block*>();
 	if (b != NULL)
 		s.insert(b);
 }
@@ -31,3 +32,4 @@ set<block*> color_block::get_set() { return s; }
 
 // 해당 객체의 같은 색 모임 숫자 반환 함수
 int color_block::get_set_size() { return s.size(); }
+int color_block::get_color() { return color; }
--- BBuyo_BBuyo/color_block.h
+++ BBuyo_BBuyo/color_block.h
@@ -6,10 +6,11 @@ class color_block
 {
 // 같은 색 모임 객체
 private:
+	int color;
 	set<class block*> s;
 public:
 	// 같은 색 모임 객체 생성자
-	color_block(class block* b);
+	color_block(class block* b, int color);
 	// 해당 객체의 같은 색 모임에 매개변수 같은 색 모임을 합하는 함수
 	void insert(set<class block*> s);
 	void remove(block* b);
@@ -17,4 +18,5 @@ public:
 	set<class block *> get_set();
 	// 해당 객체의 같은 색 모임 숫자 반환 함수
 	int get_set_size();
+	int get_color();
 };
